5df3c95ea123a53b3e1c6afff48128dbca23d13c
yovafebrian <yova.febrian512@gmail.com>
2025-10-20 14:23:43 +0700

    pembetulan pada tampilan awal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,25 +24,12 @@
             @if (Route::has('login'))
                 <nav class="flex items-center justify-end gap-4">
                     @auth
-                        <a
-                            href="{{ url('login') }}"
-                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
-                        >
-                            Login
-                        </a>
-                                                <a
-                            href="{{ url('register') }}"
-                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
-                        >
-                            register
-                        </a>
                     @else
-                        {{-- <a
-                            href="{{ route('login') }}"
-                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"
-                        >
-                            Log in
-                        </a> --}}
+                            <a
+                                href="{{ route('login') }}"
+                                class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
+                                Login
+                            </a>
 
                         @if (Route::has('register'))
                             <a
@@ -275,9 +262,5 @@
                 </div>
             </main>
         </div>
-
-        @if (Route::has('login'))
-            <div class="h-14.5 hidden lg:block">Login</div>
-        @endif
     </body>
 </html>
